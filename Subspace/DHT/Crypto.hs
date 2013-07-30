3b2fedc36b3070ece354454ec40ff75256b3c977
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2013-07-30 22:50:16 +0200

    empty Subspace/DHT/Crypto.hs

--- Subspace/DHT/Crypto.hs
+++ Subspace/DHT/Crypto.hs
@@ -1,16 +1,6 @@
 module Subspace.DHT.Crypto where
 
-import System.Random
-import Codec.Crypto.RSA
 import Subspace.DHT.Node (NodeId)
-
-data KeyPair = KeyPair (PublicKey, PrivateKey)
 
 generateNodeId :: NodeId
 generateNodeId = undefined
-
-generateIdentity :: Int
-generateIdentity = do
-  rand <- getStdGen
-  let (pubKey, privKey, g) = generateKeyPair rand 1024
-  (pubKey, privKey)
